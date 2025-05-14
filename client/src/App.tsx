1a9a27c5c3c4b436bbe516e87d4ade9a6d4fb010
Your Name <you@example.com>
2025-05-14 18:36:46 +0200

    Fix import paths for toaster components

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,8 +1,7 @@
 import { Switch, Route } from "wouter";
 import { queryClient } from "./lib/queryClient";
 import { QueryClientProvider } from "@tanstack/react-query";
-import { Toaster } from "@/components/ui/toaster";
-import { TooltipProvider } from "@/components/ui/tooltip";
+import { Toaster } from "./components/ui/toaster";
 import { ThemeProvider } from "next-themes";
 import { AuthProvider } from "@/contexts/auth-context";
 
